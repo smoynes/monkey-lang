2a7cec9d300bb63de5ba2d858409df8e65f5762e
Scott Moynes <smoynes@users.noreply.github.com>
2020-06-25 10:38:01 -0400

    Add running monkey files

---
+++ examples/data_types.monkey
@@ -0,0 +1,12 @@
+let a = 2
+puts(a)
+puts(a + 1)
+puts(a - 1)
+puts(a / 2)
+let b = "Hello"
+puts(b + " World")
+let c = []
+puts(push(c, "ok"))
+let d = {"foo":"bar"}
+puts(d["foo"])
+puts(d["bar"])
--- src/monkey/main.go
+++ src/monkey/main.go
@@ -1,12 +1,64 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
+
+	"monkey/evaluator"
+	"monkey/lexer"
+	"monkey/object"
+	"monkey/parser"
 	"monkey/repl"
 )
 
+var interactive = flag.Bool("i", false, "interactive REPL")
+
 func main() {
+	flag.Parse()
+
+	if *interactive {
+		startRepl()
+	} else {
+		if flag.NArg() == 0 {
+			os.Stderr.WriteString("no filename")
+			flag.Usage()
+		}
+		startCommand(flag.Arg(0))
+	}
+}
+
+func startRepl() {
 	fmt.Printf("Monkey language interpreter.\n")
 	repl.Start(os.Stdin, os.Stdout)
+}
+
+func startCommand(filename string) {
+	inputFile, err := os.Open(filename)
+	if err != nil {
+		os.Stderr.WriteString(filename)
+		return
+	}
+	defer inputFile.Close()
+
+	input, err := ioutil.ReadAll(inputFile)
+	env := object.NewEnvironment()
+	lexer := lexer.New(string(input))
+	parser := parser.New(lexer)
+
+	program := parser.ParseProgram()
+	if len(parser.Errors()) > 0 {
+		printParseErrors(os.Stdout, parser.Errors())
+		return
+	}
+
+	evaluator.Eval(program, env)
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
 }
